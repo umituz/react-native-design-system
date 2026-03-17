39fde6aa178c362ca3d3072e3c0652bfb5836cad
umituz <umutuz998@gmail.com>
2026-03-17 13:46:12 +0300

    chore: version patch

--- src/molecules/BaseModal.tsx
+++ src/molecules/BaseModal.tsx
@@ -41,14 +41,7 @@ export const BaseModal: React.FC<BaseModalProps> = ({
     }
   }, [dismissOnBackdrop, onClose]);
 
-  if (__DEV__) {
-    console.log("[BaseModal] Render:", { visible, testID, hasChildren: !!children });
-  }
-
   if (!visible) {
-    if (__DEV__) {
-      console.log("[BaseModal] Early returning (visible = false)");
-    }
     return null;
   }
 
